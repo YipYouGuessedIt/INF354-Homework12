aa18e7da2f92d0cf97a08df807dde53cd41c5b2b
Duran Cole <YipYouGuessedIt@users.noreply.github.com>
2015-05-21 08:52:33 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <title>Homework 11</title>   
+	</head>
+	<body>
+		<h1> Homework 11 :D </h1>
+		<body>
+			<p>
+				<?php echo Duran Nial Cole - 13329414 ?>
+			</p>
+			
+			
+		</body>
+	</body>
+</html>
